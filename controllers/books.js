0f470890372ca2926efb0045a5fcaf37d3d4b752
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-09-27 23:31:11 +0300

    create books controller for books route => MVC

---
+++ controllers/books.js
@@ -0,0 +1,182 @@
+const axios = require("axios");
+const Book = require("../models/Book");
+const { formatDate, truncate } = require("../helpers/ejs");
+
+// show add new book
+exports.showAddBook = (request, response) => {
+  console.log(request.params.data);
+  response.render("pages/books/add.ejs", { details: false });
+};
+
+// get book details from google book api with axios
+exports.bookDetails = async (req, res) => {
+  let isbn = req.body.isbn;
+  try {
+    const bookData = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`
+    );
+    if (bookData.status === 200) {
+      const data = bookData.data.items[0].volumeInfo;
+      let details = {
+        isbn: isbn,
+        title: data.title,
+        author: data.authors.join(" "),
+        cover: data.imageLinks.thumbnail.replace("http", "https"),
+      };
+      return res.render("pages/books/add.ejs", { details });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.render("pages/books/add.ejs", { details: null });
+  }
+};
+
+// show public books
+exports.publicBooks = async (request, response) => {
+  try {
+    const allBooks = await Book.find({ status: "public" })
+      .populate("user") // get the full detail of the user belongs to this book
+      .sort({ createdAt: "desc" })
+      .lean(); // return pure object
+    return response.render("pages/books/public-books", {
+      allBooks,
+      formatDate,
+      truncate,
+    });
+  } catch (error) {
+    console.error({ error });
+    return response.render("pages/errors/500");
+  }
+};
+
+// show selected book details
+exports.bookDetails = async (request, response) => {
+  try {
+    const book = await Book.findById(request.params.id)
+      .populate("user") // get the full detail of the user belongs to this book
+      .lean(); // return pure object
+    if (!book) {
+      return response.render("pages/errors/404");
+    }
+    return response.render("pages/books/book-details", {
+      book,
+      formatDate,
+      image: book.user.image,
+    });
+  } catch (error) {
+    console.error({ error });
+    return response.render("pages/errors/500");
+  }
+};
+
+// show books read by specific user
+exports.userBooks = async (request, response) => {
+  try {
+    const allBooks = await Book.find({
+      user: request.params.id,
+      status: "public",
+    })
+      .populate("user") // get the full detail of the user belongs to this book
+      .sort({ createdAt: "desc" })
+      .lean(); // return pure object
+    return response.render("pages/books/public-books", {
+      allBooks,
+      formatDate,
+      truncate,
+    });
+  } catch (error) {
+    console.error({ error });
+    return response.render("pages/errors/500");
+  }
+};
+
+// save new book to the db
+exports.saveBook = async (request, response) => {
+  // collect quotes and put them in one array quotes
+  request.body.quotes = [];
+  for (let i = 1; i <= 5; i++) {
+    request.body.quotes.push(request.body[`quote${i}`]);
+    delete request.body[`quote${i}`];
+  }
+  try {
+    // get the user id to attached it with the book model
+    request.body.user = request.user.id;
+    console.log("****", request.body);
+    await Book.create(request.body);
+    response.redirect("/dashboard");
+  } catch (error) {
+    console.error(error);
+    response.render("pages/errors/500");
+  }
+};
+
+// show EDIT book page
+exports.showEditBook = async (request, response) => {
+  try {
+    const book = await Book.findById(request.params.id).lean();
+
+    if (!book) {
+      return response.render("pages/errors/404");
+    }
+    // ensure the user logged in is the same as the book user
+    if (book.user.toJSON() !== request.user.id) {
+      response.redirect("/");
+    } else {
+      response.render("pages/books/edit.ejs", { book });
+    }
+  } catch (error) {
+    console.error(error);
+    response.render("pages/errors/500");
+  }
+};
+
+// edit the book on the db
+exports.updateBook = async (request, response) => {
+  request.body.quotes = [];
+  for (let i = 1; i <= 5; i++) {
+    request.body.quotes.push(request.body[`quote${i}`]);
+    delete request.body[`quote${i}`];
+  }
+  try {
+    const book = await Book.findById(request.params.id).lean();
+
+    if (!book) {
+      return response.render("pages/errors/404");
+    }
+    // ensure the user logged in is the same as the book user
+    if (book.user.toJSON() !== request.user.id) {
+      response.redirect("/");
+    } else {
+      console.log("***", request.body);
+      await Book.findByIdAndUpdate({ _id: request.params.id }, request.body, {
+        new: true,
+        runValidators: true,
+      });
+      response.redirect("/dashboard");
+    }
+  } catch (error) {
+    console.error(error);
+    response.render("pages/errors/500");
+  }
+};
+
+// delete a book from the db
+exports.deleteBook = async (request, response) => {
+  try {
+    const book = await Book.findById(request.params.id).lean();
+
+    if (!book) {
+      return response.render("pages/errors/404");
+    }
+    // ensure the user logged in is the same as the book user
+    if (book.user.toJSON() !== request.user.id) {
+      response.redirect("/");
+    } else {
+      await Book.findByIdAndDelete({ _id: request.params.id });
+      response.redirect("/dashboard");
+    }
+  } catch (error) {
+    console.error(error);
+    response.render("pages/errors/500");
+  }
+};
